f6b71349e34173407b390d5860d8fc212fdbefd1
arushpsingh <85397357+arushpsingh@users.noreply.github.com>
2021-07-26 17:05:30 +0530

    Update SumArray.java

--- SumArray.java
+++ SumArray.java
@@ -1,7 +1,7 @@
 public class SumArray {
   public static void main(String[] args) {
     int arr[] = { 1, 2, 3, 4 };
-    int sum = 2;
+    int sum = 0;
     for (int i = 0; i < arr.length; i++) {
       sum += arr[i];
     }
